b71bfd3123e025934a4f82aaf59f451b5303d448
flosx-talca <orlandorozasi@gmail.com>
2025-06-20 04:12:59 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const inmuebles = [
- { id: 1, direccion: 'Calle 123', precio: 150000, tipo: 'Apartamento' },
+ { id: 1, direccion: 'Calle 1223', precio: 150000, tipo: 'Apartamento' },
  { id: 2, direccion: 'Avenida 456', precio: 230000, tipo: 'Casa' },
  { id: 3, direccion: 'Carrera 789', precio: 120000, tipo: 'Estudio' }
 ];
